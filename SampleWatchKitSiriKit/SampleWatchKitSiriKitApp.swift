81657e857b12d0240c780f567b1b862660921640
Hardik Modha <hardik.modha27@gmail.com>
2026-03-25 08:31:57 +0530

    Initial Commit

---
+++ SampleWatchKitSiriKit/SampleWatchKitSiriKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWatchKitSiriKitApp.swift
+//  SampleWatchKitSiriKit
+//
+//  Created by Hardik Modha on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleWatchKitSiriKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
